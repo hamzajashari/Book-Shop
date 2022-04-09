6637e5e17de883a1149092b83faf05590e9735d0
IvanaNikolovska <74508647+IvanaNikolovska@users.noreply.github.com>
2022-04-09 18:19:31 +0200

    Add files via upload
    BookShop Domain

---
+++ BookShop/BookShop.Domain/DTO/ShoppingCartDto.cs
@@ -0,0 +1,13 @@
+﻿using BookShop.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Domain.DTO
+{
+    public class ShoppingCartDto
+    {
+        public List<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
